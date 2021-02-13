93fc2fa02e38f1461e0f6b3a5413141e5b2f30d8
syd333 <sydmiranda@icloud.com>
2021-02-13 06:10:07 -0800

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+
   def new
     @student = Student.new
   end
@@ -21,7 +22,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render "index"
   end
 
   def student_params
